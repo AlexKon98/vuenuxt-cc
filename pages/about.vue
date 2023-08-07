ba6b740c63609ff978007d3239cc22732363879a
AlexKon060698 <aleksander.kononenko2016@yandex.ru>
2023-08-08 00:10:19 +0300

    [navbar][created] - создал компонент навбара

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus est reprehenderit, eius facere doloribus, expedita error, et aspernatur iure quaerat tenetur repellat incidunt deleniti. Aliquid laborum rerum debitis nostrum?</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum accusamus magnam, illum porro, consequuntur commodi, nulla voluptate quis est dolore quaerat placeat cum molestiae quae veritatis vero reiciendis recusandae culpa?</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
